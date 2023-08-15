6ba13f02edc14fcaca7e1d6ace1de1029665fe57
LiviaBoechat <limaboechatti@gmail.com>
2023-08-15 11:23:14 -0300

    step by step

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -188,4 +188,9 @@ export default class LeaderboardService {
   }
 }
 
-// pega todas matches do model => crio array de matches, itero o total de matches e verifico se existe o time lá dentro => se não existir, crio objeto p/ ele e dou push p/ o dentro do array, se existir, faço atualização das stats => concateno todos os dois arrays (home e away) => crio novos objetos únicos para cada time => qd o objeto já foi createBrotliDecompress, somo os stats na iteração
+// pega todas matches do model
+// crio array de matches, itero o total de matches e verifico se existe o time lá dentro
+// se não existir, crio objeto p/ ele e dou push p/ o dentro do array, se existir, faço atualização das stats
+// concateno todos os dois arrays (home e away)
+// crio novos objetos únicos para cada time
+// qd o objeto unico para cada tiome já foi criado, somo os stats na iteração
